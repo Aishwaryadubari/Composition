7bfa5683b604816cfc4e01b1074a73bb1c94f6d4
Aishwarya D <60175634+Aishwaryadubari@users.noreply.github.com>
2022-08-19 16:53:53 +0530

    abstract and generic classes

--- Main.java
+++ Main.java
@@ -1,16 +1,39 @@
+
 public class Main {
 
-    public static void main(String[] args) {
-	    Dimensions dimensions = new Dimensions(20, 20, 5);
-        Case theCase = new Case("2208", "Dell", "240", dimensions);
+	public static void main(String[] args)
+	{
+		FootballPlayer joe = new FootballPlayer("Joe");
+		BaseballPlayer pat = new BaseballPlayer("pat");
+		SoccerPlayer bekham = new SoccerPlayer("Beckham");
+		
+		Team<FootballPlayer> crows = new  Team<>("Adeaide Crows");
+		crows.addPlayer(joe);
+		//crows.addPlayer(pat);
+		//crows.addPlayer(beckham);
+		System.out.println(crows.numPlayers());
+		Team<BaseballPlayer> base = new  Team<>("Chicago Cubs");
+		base.addPlayer(pat);
+		
+		Team<SoccerPlayer> soccer = new  Team<>("this wont work");
+		soccer.addPlayer(bekham);
+		
+		Team<FootballPlayer> mel = new  Team<>("Melbourne");
+		FootballPlayer banks = new FootballPlayer("Gordon");
+		
+		mel.addPlayer(banks);
+		
+		Team<FootballPlayer> hawth = new  Team<>("Hawthorn");
+		Team<FootballPlayer> freman= new  Team<>("Fremantle");
+		 hawth.matchResult(freman, 1, 0);
+	        hawth.matchResult(crows, 3, 8);
 
-        Monitor theMonitor = new Monitor("27inch Beast", "Acer", 27, new Resolution(2540, 1440));
-
-        Motherboard theMotherboard = new Motherboard("BJ-200", "Asus", 4, 6, "v2.44");
-
-        PC thePC = new PC(theCase, theMonitor, theMotherboard);
-        thePC.getMonitor().drawPixelAt(1500, 1200, "red" );
-        thePC.getMotherboard().loadProgram("Windows 1.0");
-        thePC.getTheCase().pressPowerButton();
-    }
+	        crows.matchResult(freman, 2, 1);
+	        crows.matchResult(mel, 1, 1);
+		
+		
+		
+		
+		
+	}
 }
